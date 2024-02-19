0cecf7a7dbc8277561f882ed46b3eba5f7c9ec77
Mattias <herr_mattias@hotmail.com>
2024-02-19 17:22:06 +0100

    Add boat generation function
    More efficient as no need to add a single boat at the time. Opens up for future developments allowing user to set the nr. of boats.

--- game_v_1_0.py
+++ game_v_1_0.py
@@ -30,37 +30,19 @@ def create_boat_coords(size, used_coords):
         if not overlap(used_coords, new_coords):
             return [(x, y) for x in range(x1, x2 + 1) for y in range(y1, y2 + 1)]
 
+# NEW: generate boats
+def generate_boats():
+    # empty list to track used coords
+    used_coords = []
+    # loop thru boats
+    for size in range(2, 6):
+        boat = create_boat_coords(size, used_coords)
+        for (x, y) in boat:
+            grid[y, x] = size
+        # extend list of used coords
+        used_coords.extend(boat)
+    print(grid)
+    print(used_coords)
 
-# BOATS------------------------------------------------------------------------------------------
-# boat 5
-boat_5 = create_boat_coords(5, [])
-for (x, y) in boat_5:
-    grid[y, x] = "5"
-# update used_coords with boat 5
-used_coords = boat_5[:]
+generate_boats()
 
-# boat 4
-boat_4 = create_boat_coords(4, used_coords)
-for (x, y) in boat_4:
-    grid[y, x] = "4"
-# update used_coords with boat 4
-used_coords.extend(boat_4)
-
-# boat 3
-boat_3 = create_boat_coords(3, used_coords)
-for (x, y) in boat_3:
-    grid[y, x] = "3"
-# update used_coords with boat 3
-used_coords.extend(boat_3)
-
-# boat 2
-boat_2 = create_boat_coords(2, used_coords)
-for (x, y) in boat_2:
-    grid[y, x] = "2"
-# update used_coords with boat 2
-used_coords.extend(boat_2)
-
-
-# print grid and all used coords
-print(grid)
-print(used_coords)
