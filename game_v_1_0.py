4b419ad85fa537231214565b0dd3c428289a0929
Mattias <herr_mattias@hotmail.com>
2024-02-19 18:29:13 +0100

    Add user input for grid size and nr. of boats

--- game_v_1_0.py
+++ game_v_1_0.py
@@ -2,9 +2,12 @@
 import random
 import numpy as np
 
-# grid
-grid = np.full([8, 8], ".", dtype=str)
+# NEW: user input for grid size (max 18*) and nr of boats (max 9*). *Numpy display limitations.
+grid_size = int(input("Select the grid size (max 18): "))
+nr_of_boats = int(input("Select the nr of boats (max 9): ")) + 1
 
+# grid
+grid = np.full([grid_size, grid_size], ".", dtype=str)
 
 # FUNCTIONS--------------------------------------------------------------------------------------
 # check for boat overlap
@@ -18,12 +21,12 @@ def overlap(boat_coords, new_coords):
 # create boat coords (start/end)
 def create_boat_coords(size, used_coords):
     while True:
-        x1, y1 = random.randint(0, 8 - size), random.randint(0, 8 - size)
+        x1, y1 = random.randint(0, grid_size - size), random.randint(0, grid_size - size)
         # boat direction (horizontal/vertical)
         if random.choice([True, False]):
             (x2, y2) = x1 + size - 1, y1
         else:
-            (x2, y2) = x1, y1 + size -1
+            (x2, y2) = x1, y1 + size - 1
 
         # boat coords (middle)
         new_coords = [(x, y) for x in range(x1, x2 + 1) for y in range(y1, y2 + 1)]
@@ -35,7 +38,7 @@ def generate_boats():
     # empty list to track used coords
     used_coords = []
     # loop thru boats
-    for size in range(2, 6):
+    for size in range(nr_of_boats):
         boat = create_boat_coords(size, used_coords)
         for (x, y) in boat:
             grid[y, x] = size
@@ -45,4 +48,3 @@ def generate_boats():
     print(used_coords)
 
 generate_boats()
-
